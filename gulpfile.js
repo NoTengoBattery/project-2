12d400ded2d3f4856f5b19ef19169dcb7ee5c579
Oever González <notengobattery@gmail.com>
2020-11-30 10:23:23 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/*
+ * GULP build file to automatically prefix CSS files when they change
+ */
+
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function (done) {
+  'use strict';
+  gulp.src('src/css/**/*.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build/css'));
+  done();
+});
+
+gulp.task('watch', function () {
+  'use strict';
+  gulp.watch('css/**/*.css', gulp.series('styles'));
+});
