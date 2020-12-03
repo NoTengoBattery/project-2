2509dc9fa4440834900ade1e49ffbb1ad31cb8db
Oever González <notengobattery@gmail.com>
2020-12-03 13:38:37 -0600

    project: ignore intermeditate compilation files

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,20 @@
 
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var cleancss = require('gulp-clean-css');
+var purgecss = require('gulp-purgecss');
 
 gulp.task('styles', function (done) {
   'use strict';
   gulp.src('src/**/*.css')
     .pipe(autoprefixer())
+    .pipe(purgecss({
+      content: ['src/**/*.html', 'index.html']
+    }))
+    .pipe(cleancss({
+      format: 'keep-breaks',
+      level: 2
+    }))
     .pipe(gulp.dest('build'));
   done();
 });
